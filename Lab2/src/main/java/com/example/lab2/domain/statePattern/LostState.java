bd01d2cbb18badb7bc004338a7c49f7a68d338d3
Anniegavr <56108881+Anniegavr@users.noreply.github.com>
2021-12-24 01:50:19 +0200

    95%

--- Lab2/src/main/java/com/example/lab2/domain/statePattern/LostState.java
+++ Lab2/src/main/java/com/example/lab2/domain/statePattern/LostState.java
@@ -1,4 +1,25 @@
 package com.example.lab2.domain.statePattern;
 
-public class LostState {
+public class LostState extends State{
+    LostState(Player player){
+        super(player);
+    }
+
+    @Override
+    public String gameOn() {
+        return null;
+    }
+
+    @Override
+    public String gameWon() {
+        return null;
+    }
+
+    @Override
+    public String gameLost() {
+        player.setGameOn(false);
+        player.setHangCurrentState("\n +---+\n |   |\n O   |\n/|\\  |\n/ \\  |\n     |\nTTTTTT");
+        player.setCurrentStateInformation("You lost!");
+        return player.getCurrentStateInformation();
+    }
 }
