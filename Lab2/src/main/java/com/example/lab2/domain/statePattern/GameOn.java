c3e9ff546c9b1464cc869255a38402f9f0334818
Anniegavr <56108881+Anniegavr@users.noreply.github.com>
2021-12-24 01:47:56 +0200

    95%

--- Lab2/src/main/java/com/example/lab2/domain/statePattern/GameOn.java
+++ Lab2/src/main/java/com/example/lab2/domain/statePattern/GameOn.java
@@ -9,11 +9,34 @@ public class GameOn extends State {
 
     GameOn(Player player) {
         super(player);
+        player.setGameOn(true);
+        player.setLives(6);
     }
 
     @Override
     public String gameOn() {
         player.setCurrentStateInformation("You have "+player.getLives()+" lives left\n"+player.getGuessedLetters()+player.getHangCurrentState());
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()){
+            int tryNo = 0;
+            String currentLetter = scanner.nextLine();
+            if(currentLetter.equals(String.valueOf(player.getWordToBeGuessed().charAt(tryNo)))){
+                player.addCorrectLetter(currentLetter);
+                if(player.getWordToBeGuessed().length()==player.getGuessedLetters().size()){
+                    WinState ws = new WinState(player);
+                    player.changeState(new WinState(player));
+                    return ws.gameWon();
+                }else{
+                    gameOn();
+                }
+            }else{
+                DownState ds = new DownState(player);
+                if(!ds.gameLost().equals("You lost!")){
+                    gameOn();
+                }
+                return player.getCurrentStateInformation();
+            }
+        }
         return player.getCurrentStateInformation();
     }
 
